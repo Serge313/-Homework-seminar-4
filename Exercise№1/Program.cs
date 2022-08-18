a616526c50dadbbe1abe87a4e6bdeea31c8898b3
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-08-18 11:04:57 +0300

    Exercise№1 completed

---
+++ Exercise№1/Program.cs
@@ -0,0 +1,39 @@
+﻿//Решение с помощью метода
+double firstNumber = GetNumber("Enter your first number ");
+double secondNumber = GetNumber("Enter your second number ");
+double numberToPower = ToPower(firstNumber, secondNumber);
+Console.WriteLine($"{firstNumber}, {secondNumber} -> {numberToPower}");
+
+
+double ToPower(double a, double b)
+{
+    double c = Math.Pow(a, b);
+    return c;
+}
+
+
+double GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+
+//Решение с помощью цикла
+int number1 = GetYourNumber("Enter number 1 ");
+int number2 = GetYourNumber("Enter number 2 ");
+int toPower = 1;
+
+
+for (int i = 0; i < number2; i++)
+{
+    toPower = toPower * number1;
+}
+Console.WriteLine(toPower);
+
+
+int GetYourNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
