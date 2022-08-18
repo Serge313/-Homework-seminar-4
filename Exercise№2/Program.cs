e68d384d46c4dfa8a77112867aced5fb76ef86a2
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-08-18 11:31:21 +0300

    Exercise№2 completed

---
+++ Exercise№2/Program.cs
@@ -0,0 +1,19 @@
+﻿int number = GetNumber("Enter your number ");
+string length = number.ToString();
+int sum = 0;
+
+
+for (int i = 0; i < length.Length; i++)
+{
+    int digit = number % 10;
+    number = number / 10;
+    sum = sum + digit;
+}
+Console.WriteLine(sum);
+
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
