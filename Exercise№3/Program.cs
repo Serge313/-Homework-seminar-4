6a3de80b3bbdc955f342ea55eaf4a01c85741c51
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-08-22 10:03:13 +0300

    Exercise№3 completed

---
+++ Exercise№3/Program.cs
@@ -0,0 +1,21 @@
+﻿int size = GetNumber("Enter your number ");
+int[] array = new int [size];
+FillArray(array);
+
+
+void FillArray(int[] array1) 
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+        array1[i] = new Random().Next(1, 101);
+        Console.Write(array1[i] + " ");
+    }
+}
+
+
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
